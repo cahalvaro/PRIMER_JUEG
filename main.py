0f1a5d31beeb5e611d7aa65d8fa944469b4b7a39
Alvaro <caalvaro123@gmail.com>
2024-10-05 20:36:59 -0400

    Cambios

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
+import Constantes
+from personajes import Personaje
+
+jugador=Personaje(50,50)
 
 pygame.init()
-#pantalla
-ancho=800
-alto=600
 
-ventana=pygame.display.set_mode((ancho,alto))
+ventana=pygame.display.set_mode((Constantes.ANCHO_VENTANA,Constantes.ALTO_VENTANA))
+pygame.display.set_caption("Mi primer juego")
 run=True
 while run==True:
+    jugador.dibujar(ventana)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+
+    pygame.display.update()
 
 pygame.quit()
---
+++ personajes.py
@@ -0,0 +1,10 @@
+import pygame
+import Constantes
+
+class Personaje():
+    def __init__(self,x, y):
+        self.forma=pygame.Rect(0, 0, Constantes.ANCHO_PERSONAJE, Constantes.ALTO_PERSONAJE)
+        self.forma.center=(x,y)
+
+    def dibujar(self, interfaz):
+        pygame.draw.rect(interfaz,Constantes.COLOR_PERSONAJE, self.forma)
