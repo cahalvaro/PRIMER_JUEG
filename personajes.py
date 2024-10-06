270a0798c023f451d4d1533da8739d29594fd98c
Alvaro <caalvaro123@gmail.com>
2024-10-05 21:06:09 -0400

    Cambios1

--- personajes.py
+++ personajes.py
@@ -2,6 +2,7 @@ import pygame
 import Constantes
 
 class Personaje():
+    
     def __init__(self,x, y):
         self.forma=pygame.Rect(0, 0, Constantes.ANCHO_PERSONAJE, Constantes.ALTO_PERSONAJE)
         self.forma.center=(x,y)
